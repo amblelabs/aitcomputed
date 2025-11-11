3e4890e1af5c96eabeb863f4f3d9a4bcbab81fde
VenWhoVian <jackconnan11@gmail.com>
2025-11-11 04:49:20 -0500

    Update ComputedBlocks.java

--- src/main/java/mc/duzo/aitcompute/registry/ComputedBlocks.java
+++ src/main/java/mc/duzo/aitcompute/registry/ComputedBlocks.java
@@ -6,5 +6,5 @@ import mc.duzo.aitcompute.registry.blocks.VortexCommunicatorBlock;
 import net.minecraft.block.Block;
 
 public class ComputedBlocks extends BlockContainer {
-    public static final Block VORTEX_COMMUNICATOR = new VortexCommunicatorBlock(ABlockSettings.create().burnable());
+    public static final Block VORTEX_COMMUNICATOR = new VortexCommunicatorBlock(ABlockSettings.create());
 }
